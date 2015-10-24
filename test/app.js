2a369c0d3dd4f68b11fc46c1a342ec2436a351af
Phil <birkal@outlook.com>
2015-10-24 12:04:35 -0700

    Update tests

---
+++ test/app.js
@@ -0,0 +1,36 @@
+var request = require('supertest');
+
+var app = require('../server.js');
+
+describe('GET /', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
+
+describe('GET /404', function() {
+  it('should return 404 Page Not Found', function(done) {
+    request(app)
+      .get('/404')
+      .expect(404, done);
+  });
+});
+
+describe('GET /signup', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/signup')
+      .expect(200, done);
+  });
+});
+
+describe('GET /login', function() {
+  it('should return 200 OK', function(done) {
+    request(app)
+      .get('/login')
+      .expect(200, done);
+  });
+});
+
